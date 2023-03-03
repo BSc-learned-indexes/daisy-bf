4eeadce1e2725f5d6090297084a447c4e4d07606
birkj <jens.birk.andersen@gmail.com>
2023-03-03 16:44:12 +0100

    fix path

---
+++ experiment/intro.py
@@ -0,0 +1,7 @@
+from termcolor import colored
+from pyfiglet import Figlet
+
+# print(colored(f.renderText('Hello'), 'red'))
+              
+f = Figlet (font='slant' )
+print(colored(f. renderText ('Daisy Benchmark'), 'green' ))

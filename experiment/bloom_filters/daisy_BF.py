480e109b684d2d79e25ea9e533aa97702a300ad4
birkj <jens.birk.andersen@gmail.com>
2023-03-10 16:21:19 +0100

    daisy kinda works

--- experiment/bloom_filters/daisy_BF.py
+++ experiment/bloom_filters/daisy_BF.py
@@ -14,8 +14,8 @@ def init_args():
                         help="path of the false positive ratings")
     parser.add_argument('--out_path', action="store", dest="out_path", type=str,
                         required=False, help="path of the output", default="./data/plots/")
-    parser.add_argument('--const_q', action="store", dest="const_q", type=bool,
-                        required=False, help="make q a constant", default=True)
+    parser.add_argument('--const_qx', action="store", dest="const_qx", type=bool,
+                        required=False, help="make qx a constant", default=True)
 
     parser.parse_args()
     return parser.parse_args()
@@ -28,13 +28,14 @@ def load_data():
     return data, positive_sample, negative_sample
 
 def choose_number_of_hash_functions(n, F, px, qx):
-    if px > (1 / n): 
+    if qx <= F * px or px > (1 / n): 
         k_x = 0
+        print("yo")
     elif F * px < qx and qx <= min(px, (F/n)):
-        k_x = math.log(1/F*(qx/px), 2)
+        k_x = math.log(1/(F*(qx/px)), 2)
     elif qx > px and (F/n) >= px:
         k_x = math.log(1/F, 2)
-    elif qx > (F/n) and (F/n) < px and px <= 1/n:
+    elif qx > (F/n) and ((F/n) < px and px <= 1/n):
         k_x = math.log(1/(n*px), 2)
     else: 
         raise Exception("k could not be calculated.")
@@ -42,26 +43,26 @@ def choose_number_of_hash_functions(n, F, px, qx):
     return math.ceil(k_x)
     
 def calc_lower_bound(data, F, n):
-    #u_2, u_3, u_4 = 0, 0, 0
     total = 0
+    print(data.head())
+    standard_k = math.log((1/F), 2)
+    for _, row in data.iterrows():
+        px = row["px"]
+        qx = row["qx"] 
+        num_k = choose_number_of_hash_functions(n, F, px, qx)
+        print(num_k, standard_k)
+        total += px * num_k
+    return n * total
 
-    for row in data.rows:
-        
-        total += choose_number_of_hash_functions(n, F, )
-
-
-    
-
-def calc_filter_size_from_target_FPR(data, F, n):
-    lower_bound = calc_lower_bound(data, F, n)
-    return math.log(math.e, 2) * lower_bound
+def calc_filter_size_from_target_FPR(data, F_target, n):
+    lower_bound = calc_lower_bound(data, F_target, n)
+    size = math.log(math.e, 2) * lower_bound
+    return math.ceil(size)
 
 def normalize_scores(pos_data, neg_data):
-    k = pos_data["score"].sum() + neg_data["score"].sum()
-    # n = len(pos_data["score"]) + len(neg_data)
-
-    pos_data["norm_score"] = pos_data["score"].div(k)
-    neg_data["norm_score"] = neg_data["score"].div(k)
+    score_sum = pos_data["score"].sum() + neg_data["score"].sum()
+    pos_data["px"] = pos_data["score"].div(score_sum)
+    neg_data["px"] = neg_data["score"].div(score_sum)
 
 if __name__ == '__main__':
 
@@ -69,23 +70,28 @@ if __name__ == '__main__':
     DATA_PATH = args.data_path
     FPR_PATH = args.fpr_data_path
     OUT_PATH = args.out_path
-    CONST_Q = args.const_q
+    CONST_QX = args.const_qx
 
     #Progress bar 
     # bar = Bar('Creating Daisy-BF', max=math.ceil((args.max_size - args.min_size)/args.step))
 
     all_data, positive_data, negative_data = load_data()
 
-    # create constant Q:
-    Q = 0.0
-    if CONST_Q:
-        Q = 1 / (len(negative_data) + len(positive_data))
-        print(f"Q is set to be constant: {Q}")
-
+    # create constant qx:
+    qx = 0.0
+    if CONST_QX:
+        qx = 1 / (len(negative_data) + len(positive_data))
+        all_data["qx"] = qx 
+        positive_data["qx"] = qx 
+        negative_data["qx"] = qx 
+        print(f"qx is set to be constant: {qx}")
+    
     normalize_scores(positive_data, negative_data)
 
     print(positive_data.head())
     print(negative_data.head())
 
+    size = calc_filter_size_from_target_FPR(pd.concat([positive_data, negative_data]), 0.0001, len(positive_data))
+    print(size)
     mem_arr = []
     FPR_arr = []

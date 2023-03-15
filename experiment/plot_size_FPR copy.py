259b69561f545ef737e4802bf5d6dd36297324ab
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2023-03-15 20:55:25 +0100

    add blackboard daisy, export to csv and making of plots

--- experiment/plot_size_FPR copy.py
+++ experiment/plot_size_FPR copy.py
@@ -28,10 +28,16 @@ for name in names:
     # Plot distribution of Keys
     bar.next()
     x = data["memory"]
-    x = x.div(1000)
+    #x = x.div(1000)
     y = data["false_positive_rating"]
 
     plt.plot(x, y, linestyle='dashed', marker='x')
+
+x = [4205335, 3305476, 3033411, 2879589, 2732328, 2631061, 2581260, 2485739, 2431056, 2401464, 2331858]
+x = x / 1000
+
+y = [5e-05, 0.000395, 0.00074, 0.001085, 0.00143, 0.001775, 0.00212, 0.0024649999999999997, 0.0028099999999999996, 0.0031549999999999994, 0.003499999999999999]
+plt.plot(x, y, linestyle='dashed', marker='x')
 
 # plt.yscale("log")
 plt.legend(names)
